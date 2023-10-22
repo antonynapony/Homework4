e4630133ae87229e9665df8af8f223190e14758d
antonynapony <tonypoltorabatko@gmail.com>
2023-10-22 14:47:26 +0300

    Add Code to Hometask4.1

--- Hometask4.1/Program.cs
+++ Hometask4.1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+
+while (true)
+{
+    bool isParseSuccess = int.TryParse(Console.ReadLine(), out int arrayLength);
+    if (isParseSuccess)
+    {
+        int[] numbers = new int[arrayLength];
+        int sum = 0;
+        for (int i = 0; i < arrayLength; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+            sum += numbers[i];
+        }
+
+        Console.Write($"Сумма элементов массива равна: {sum}");
+    }
+    else
+    {
+        Console.WriteLine("Неверно введено число!");
+    }
+}
